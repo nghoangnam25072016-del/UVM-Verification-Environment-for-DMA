82df371b1ad5bdc6954cc858134e45a1ac02cedb
nghoangnam25072016-del <nghoangnam25072016@gmail.com>
2026-04-29 09:31:19 -0700

    Update dma_if.sv

--- tb/dma_if.sv
+++ tb/dma_if.sv
@@ -1,1 +1,28 @@
+interface dma_if(input logic clk);
 
+    logic rst_n;
+    logic start;
+    logic [7:0] length;
+    logic busy;
+    logic dma_done;
+
+    logic [31:0] araddr;
+    logic        arvalid;
+    logic        arready;
+
+    logic [31:0] rdata;
+    logic        rvalid;
+    logic        rready;
+
+    logic [31:0] awaddr;
+    logic        awvalid;
+    logic        awready;
+
+    logic [31:0] wdata;
+    logic        wvalid;
+    logic        wready;
+
+    logic        bvalid;
+    logic        bready;
+
+endinterface
